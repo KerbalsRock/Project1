1a6bff298d8b4f89a01c7a1f65931d712fcc2282
KerbalsRock <bbishop19@windwardschool.org>
2016-08-30 08:21:43 -0700

    Testing out GitHub
    buHtiG tuo gnitseT

---
+++ src/Class1.java
@@ -0,0 +1,10 @@
+
+public class Class1 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Ocean Man");
+		System.out.println("Take me by the hand");
+		System.out.println("Lead me to the land that you understand");	
+	}
+}
